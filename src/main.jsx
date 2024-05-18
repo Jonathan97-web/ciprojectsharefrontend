05f54a64760bac544b5387b48bae0d0f852ee72a
jonathan97-web <zakrisson97@gmail.com>
2024-05-18 16:45:36 +0200

    Add router to main.jsx and add context providers

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { LoggedInUserProvider } from "./context/LoggedInUserContext.jsx";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
+import NavBar from "./components/NavBar.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: (
+      <div>
+        Error! <br /> Page Not Found! <br />
+        <button>Home</button>
+      </div>
+    ),
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <LoggedInUserProvider>
+      <NavBar />
+      <RouterProvider router={router} />
+    </LoggedInUserProvider>
+  </React.StrictMode>
+);
