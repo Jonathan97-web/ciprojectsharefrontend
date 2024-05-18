2e258e55c15d836385a1fa5f45949250032ddd63
jonathan97-web <zakrisson97@gmail.com>
2024-05-18 23:24:23 +0200

    Refactor router to better suit the needs of the application

--- src/main.jsx
+++ src/main.jsx
@@ -2,38 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { LoggedInUserProvider } from "./context/LoggedInUserContext.jsx";
 import Login from "./pages/Login.jsx";
 import Signup from "./pages/Signup.jsx";
 import NavBar from "./components/NavBar.jsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: (
-      <div>
-        Error! <br /> Page Not Found! <br />
-        <button>Home</button>
-      </div>
-    ),
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/signup",
-    element: <Signup />,
-  },
-]);
+import ProfileDetail from "./pages/ProfileDetail.jsx";
+import ProjectDetail from "./pages/ProjectDetail.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <LoggedInUserProvider>
-      <NavBar />
-      <RouterProvider router={router} />
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<App />} errorElement={<div>Error</div>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/profile/:id" element={<ProfileDetail />} />
+          <Route path="/project/:id" element={<ProjectDetail />} />
+          <Route path="*" element={<div>Not Found</div>} />
+        </Routes>
+      </Router>
     </LoggedInUserProvider>
   </React.StrictMode>
 );
