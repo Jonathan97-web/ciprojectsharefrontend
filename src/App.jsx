e86f269fd201bf5a09ec57224bbce8dd26e5eb6c
jonathan97-web <zakrisson97@gmail.com>
2024-05-18 23:24:05 +0200

    Refactor handleSignOut to own hook and implement fetchprojects

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,43 @@
 import "./App.css";
-import {
-  useLoggedInUser,
-  useSetLoggedInUser,
-} from "./context/LoggedInUserContext";
-import { useNavigate } from "react-router-dom";
+import { useLoggedInUser } from "./context/LoggedInUserContext";
+import ProjectCard from "./components/ProjectCard";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [loading, setLoading] = useState(false);
-  const setLoggedInUser = useSetLoggedInUser();
   const loggedInUser = useLoggedInUser();
-  const navigate = useNavigate();
+  const [projects, setProjects] = useState([]);
 
-  useEffect(() => {
-    if (!loggedInUser) {
-      setLoading(true);
-    } else {
-      setLoading(false);
-    }
-  }, [loggedInUser]);
-
-  const handleSignOut = async () => {
-    try {
-      await axios.post("http://localhost:8000/dj-rest-auth/logout/");
-      localStorage.removeItem("token");
-      localStorage.removeItem("refresh");
-      setLoggedInUser(null);
-    } catch (err) {
-      console.log(err);
-    }
+  const fetchProjects = async () => {
+    const data = await axios.get("http://localhost:8000/projects/");
+    console.log(data.data);
+    setProjects(data.data);
   };
 
-  if (loading) {
-    return <h1>Loading...</h1>;
-  }
+  useEffect(() => {
+    fetchProjects();
+  }, []);
 
   return (
     <>
-      {!loggedInUser ? (
+      {!loggedInUser && (
         <>
           <h1>Welcome!</h1>
           <h2>To the Community-Sweden sharing website for Projects</h2>
-          <button onClick={() => navigate("/login")}>Login</button>
-          <button onClick={() => navigate("/signup")}>Sign Up</button>
-        </>
-      ) : (
-        <>
-          <h1>
-            Welcome {loggedInUser.username ? loggedInUser.username : "Guest"}!
-          </h1>
-          <div>You are logged in</div>
-          <button onClick={handleSignOut}>Logout</button>
         </>
       )}
+      <>
+        {loggedInUser && (
+          <>
+            <h1>Welcome {loggedInUser?.username}!</h1>
+            <div>You are logged in</div>
+
+            {projects.map((project, id) => (
+              <ProjectCard key={id} project={project} />
+            ))}
+          </>
+        )}
+      </>
     </>
   );
 }
