0c69e735f1aea2fed6a5ab9b273bc61c50cd07ce
jonathan97-web <zakrisson97@gmail.com>
2024-05-21 14:34:04 +0200

    Create AddProject.jsx file for form for adding projects

---
+++ src/pages/Projects/AddProject.jsx
@@ -0,0 +1,53 @@
+import { useEffect } from "react";
+import useProject from "../../hooks/useProject";
+
+export default function AddProject() {
+  const {
+    error,
+    setAddProject,
+    handleSubmit,
+    repo,
+    homepage,
+    title,
+    description,
+    handleChange,
+  } = useProject();
+
+  useEffect(() => {
+    setAddProject(true);
+  });
+
+  return (
+    <>
+      <form className="flex flex-col  gap-5" onSubmit={handleSubmit}>
+        <div className="flex flex-col items-start">
+          Title {error && <p>{error.title}</p>}
+        </div>
+        <input type="text" name="title" value={title} onChange={handleChange} />
+        <div className="flex flex-col items-start">
+          Description {error && <p>{error.description}</p>}
+        </div>
+        <input
+          type="text"
+          name="description"
+          value={description}
+          onChange={handleChange}
+        />
+        <div className="flex">
+          <p>Homepage</p>
+        </div>
+        <input
+          type="text"
+          name="homepage"
+          value={homepage}
+          onChange={handleChange}
+        />
+        <div className="flex flex-col items-start">
+          Repo {error && <p>{error.repo}</p>}
+        </div>
+        <input type="text" name="repo" value={repo} onChange={handleChange} />
+        <button type="submit">submit</button>
+      </form>
+    </>
+  );
+}
